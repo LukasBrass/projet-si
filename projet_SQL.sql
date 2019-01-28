0a9b5bbe79b42f849d07ddebe0b63c96127b2c13
BRASSELEUR Lukas <lbrasseleur@D30-07.local>
2019-01-28 14:42:58 +0100

    Update and finish SQL

--- projet_SQL.sql
+++ projet_SQL.sql
@@ -1,103 +1,109 @@
-#------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
 
-#------------------------------------------------------------
-# Table: Consultant
-#------------------------------------------------------------
-
-CREATE TABLE Consultant(
-        id          Int  Auto_increment  NOT NULL ,
-        code        Varchar (50) NOT NULL ,
-        name_c      Varchar (50) NOT NULL ,
-        firstname_c Varchar (50) NOT NULL ,
-        phone       Varchar (50) NOT NULL ,
-        mail        Varchar (50) NOT NULL ,
-        hired_date  Date NOT NULL ,
-        type_metier Varchar (5) NOT NULL ,
-        specialite  Varchar (50) DEFAULT NULL ,
-        TJM_Ing     Int DEFAULT NULL,
-        TJM_CP      Int DEFAULT NULL
-	,CONSTRAINT Consultant_PK PRIMARY KEY (id)
-)ENGINE=InnoDB;
-
-#------------------------------------------------------------
-# Table: Manager
-#------------------------------------------------------------
-
-CREATE TABLE Manager(
-        id Int  Auto_increment  NOT NULL
-	,CONSTRAINT Manager_PK PRIMARY KEY (id)
-)ENGINE=InnoDB;
+CREATE TABLE `Consultant` (
+  `id` int(11) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `name_c` varchar(50) NOT NULL,
+  `firstname_c` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `hired_date` date NOT NULL,
+  `type_metier` varchar(5) NOT NULL,
+  `specialite` varchar(50) DEFAULT NULL,
+  `TJM_Ing` int(11) DEFAULT NULL,
+  `TJM_CP` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-#------------------------------------------------------------
-# Table: Projet
-#------------------------------------------------------------
-
-CREATE TABLE Projet(
-        id         Int  Auto_increment  NOT NULL ,
-        start_date Date NOT NULL ,
-        end_date   Date NOT NULL ,
-        id_Manager Int NOT NULL
-	,CONSTRAINT Projet_PK PRIMARY KEY (id)
-
-	,CONSTRAINT Projet_Manager_FK FOREIGN KEY (id_Manager) REFERENCES Manager(id)
-)ENGINE=InnoDB;
+CREATE TABLE `EquipeProjet` (
+  `id_consultant` int(11) NOT NULL,
+  `id_projet` int(11) NOT NULL,
+  `workedDays` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-#------------------------------------------------------------
-# Table: EquipeProjet
-#------------------------------------------------------------
+CREATE TABLE `Manager` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE EquipeProjet(
-        id        Int NOT NULL ,
-        id_Projet Int NOT NULL
-	,CONSTRAINT EquipeProjet_PK PRIMARY KEY (id,id_Projet)
+CREATE TABLE `Projet` (
+  `id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `id_Manager` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	,CONSTRAINT EquipeProjet_Consultant_FK FOREIGN KEY (id) REFERENCES Consultant(id)
-	,CONSTRAINT EquipeProjet_Projet0_FK FOREIGN KEY (id_Projet) REFERENCES Projet(id)
-)ENGINE=InnoDB;
-
-
-#populate database : 
-# Add ingénieurs
-
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, specialite, TJM_Ing) VALUES ("code1", "name1","firstname1","0606060606","name1@gmail.com", "2019-01-01","I", "etudes", "200");
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, specialite, TJM_Ing) VALUES ("code2", "name2","firstname2","0606060606","name2@gmail.com", "2019-01-01","I", "developpement", "200");
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, specialite, TJM_Ing) VALUES ("code3", "name3","firstname3","0606060606","name3@gmail.com", "2019-01-01","I", "etudes", "200");
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, specialite, TJM_Ing) VALUES ("code4", "name4","firstname4","0606060606","name4@gmail.com", "2019-01-01","I", "developpement", "200");
-
-#Add Chef de projets
-
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, TJM_CP) VALUES ("code5", "name5","firstname5","0606060606","name5@gmail.com", "2019-01-01","P", 250);
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, TJM_CP) VALUES ("code6", "name6","firstname6","0606060606","name6@gmail.com", "2019-01-01","P", 250);
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, TJM_CP) VALUES ("code3", "name7","firstname7","0606060606","name7@gmail.com", "2019-01-01","P", 250);
-insert into Consultant (code, name_c, firstname_c, phone, mail, hired_date, type_metier, TJM_CP) VALUES ("code8", "name8","firstname8","0606060606","name8@gmail.com", "2019-01-01","P", 250);
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `login` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-# Add Managers
+ALTER TABLE `Consultant`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Consultant_User_FK` (`id`) USING BTREE;
 
-insert into Manager values();
-insert into Manager values();
-insert into Manager values();
+ALTER TABLE `EquipeProjet`
+  ADD PRIMARY KEY (`id_consultant`,`id_projet`) USING BTREE,
+  ADD KEY `EquipeProjet_Projet_FK` (`id_projet`) USING BTREE,
+  ADD KEY `EquipeProjet_Consultant_FK` (`id_consultant`);
+
+ALTER TABLE `Manager`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Manager_User_FK` (`id`) USING BTREE;
+
+ALTER TABLE `migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+ALTER TABLE `Projet`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Projet_Manager_FK` (`id_Manager`);
+
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
 
 
-#Add Projets
+ALTER TABLE `Manager`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
-insert into Projet(start_date, end_date, id_Manager) values ("2019-01-01", "2019-02-01",1);
-insert into Projet(start_date, end_date, id_Manager) values ("2018-01-01", "2018-02-01",1);
-insert into Projet(start_date, end_date, id_Manager) values ("2019-10-01", "2018-11-01",2);
-insert into Projet(start_date, end_date, id_Manager) values ("2018-12-01", "2019-02-01",2);
-insert into Projet(start_date, end_date, id_Manager) values ("2019-01-01", "2019-02-02",3);
-insert into Projet(start_date, end_date, id_Manager) values ("2018-07-01", "2018-08-01",3);
+ALTER TABLE `Projet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
-#Add consultants in projects
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
-insert into EquipeProjet(id, id_projet) values (1,1);
-insert into EquipeProjet(id, id_projet) values (1,2);
-insert into EquipeProjet(id, id_projet) values (2,1);
-insert into EquipeProjet(id, id_projet) values (2,2);
-insert into EquipeProjet(id, id_projet) values (3,2);
-insert into EquipeProjet(id, id_projet) values (3,3);
 
+ALTER TABLE `Consultant`
+  ADD CONSTRAINT `Consultant_User_FK` FOREIGN KEY (`id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `EquipeProjet`
+  ADD CONSTRAINT `EquipeProjet_Consultant_FK` FOREIGN KEY (`id_consultant`) REFERENCES `Consultant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `EquipeProjet_Projet_FK` FOREIGN KEY (`id_projet`) REFERENCES `Projet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `Manager`
+  ADD CONSTRAINT `Manager_User_FK` FOREIGN KEY (`id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `Projet`
+  ADD CONSTRAINT `Projet_Manager_FK` FOREIGN KEY (`id_Manager`) REFERENCES `Manager` (`id`);
+
+INSERT INTO `Projet` (`id`, `start_date`, `end_date`, `id_Manager`) VALUES
+(1, '2019-01-01', '2019-01-15', 1),
+(2, '2019-01-15', '2019-01-31', 1);
+
+INSERT INTO `User` (`id`, `login`, `password`) VALUES
+(1, 'login1', 'password1'),
+(2, 'login2', 'password2'),
+(3, 'login3', 'password3'),
+(4, 'login4', 'password4'),
+(5, 'login5', 'password5');
+
+INSERT INTO `Consultant` (`id`, `code`, `name_c`, `firstname_c`, `phone`, `mail`, `hired_date`, `type_metier`, `specialite`, `TJM_Ing`, `TJM_CP`) VALUES
+(2, 'code2', 'name2', 'firstname2', '0160606060', 'test2@gmail.com', '2018-11-13', 'I', 'etudes', 200, NULL),
+(3, 'code3', 'name3', 'firstname3', '0160606061', 'test3@gmail.com', '2018-10-18', 'P', NULL, NULL, 250);
+
+INSERT INTO `Manager` (`id`) VALUES
+(1);
+
+INSERT INTO `EquipeProjet` (`id_consultant`, `id_projet`, `workedDays`) VALUES
+(2, 1, 20),
+(3, 2, 0);
